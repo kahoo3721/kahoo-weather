2a76be089181cacdef7a8af00c520da764d834a1
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-13 22:37:28 +0900

    スタンプ

--- index.php
+++ index.php
@@ -142,35 +142,25 @@ foreach ($events as $event) {
     // 天気情報、更新時刻、晴れのスタンプをまとめて送信
     replyMultiMessage($bot, $event->getReplyToken(),
       new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($json['description']['text'] . PHP_EOL . PHP_EOL .
-        '最終更新：' . sprintf('%s月%s日%s時%s分', $date['month'], $date['day'], $date['hour'], $date['minute'])));
-        replyImageMessage($bot, $event->getReplyToken(), 'http://' .
-                    $_SERVER['HTTP_HOST'] .
-                    '/imgs/pmpm-sunny.jpg-large',
-                    'http://' . $_SERVER['HTTP_HOST'] .
-                    '/imgs/pmpm-sunny.jpg-large');
-
+        '最終更新：' . sprintf('%s月%s日%s時%s分', $date['month'], $date['day'], $date['hour'], $date['minute'])),
+      new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(2, 513)
+    );
   // 雨の場合
   } else if($json['forecasts'][0]['telop'] == '雨') {
     replyMultiMessage($bot, $event->getReplyToken(),
       // 天気情報、更新時刻、雨のスタンプをまとめて送信
       new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($json['description']['text'] . PHP_EOL . PHP_EOL .
-        '最終更新：' . sprintf('%s月%s日%s時%s分', $date['month'], $date['day'], $date['hour'], $date['minute'])));
-    replyImageMessage($bot, $event->getReplyToken(), 'http://' .
-                $_SERVER['HTTP_HOST'] .
-                '/imgs/pmpm-rain.jpg-large',
-                'http://' . $_SERVER['HTTP_HOST'] .
-                '/imgs/pmpm-rain.jpg-large');
-
+        '最終更新：' . sprintf('%s月%s日%s時%s分', $date['month'], $date['day'], $date['hour'], $date['minute'])),
+      new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(2, 507)
+    );
   // 他
   } else {
     // 天気情報と更新時刻をまとめて返信
     replyTextMessage($bot, $event->getReplyToken(), $json['description']['text'] . PHP_EOL . PHP_EOL .
       '最終更新：' . sprintf('%s月%s日%s時%s分', $date['month'], $date['day'], $date['hour'], $date['minute']));
-      replyImageMessage($bot, $event->getReplyToken(), 'http://' .
-                  $_SERVER['HTTP_HOST'] .
-                  '/imgs/pmpm-other.jpg-large',
-                  'http://' . $_SERVER['HTTP_HOST'] .
-                  '/imgs/pmpm-other.jpg-large');
+        replyMultiMessage($bot, $event->getReplyToken(),
+        new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(2, 507)
+      );
   }
 }
 // テキストを返信。引数はLINEBot、返信先、テキスト
